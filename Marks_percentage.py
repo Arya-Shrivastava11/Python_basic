db942468c1241e813d764a55b60651ff969fdc04
Arya-Shrivastava11 <aryashrivastava29@gmail.com>
2026-09-23 16:11:43 +0530

    Add files via upload

---
+++ Marks_percentage.py
@@ -0,0 +1,8 @@
+a=float(input("Enter student's marks"))
+b=float(input(""))
+c=float(input(""))
+d=float(input(""))
+e=float(input(""))
+Total_percentage=100*(a+b+c+d+e)/500
+print(f"Percentage={a},{b},{c},{d},{e}")
+print(Total_percentage)
