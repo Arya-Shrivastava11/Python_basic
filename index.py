4f755f1dabbe048d8ce58f57d566aa825f5f8c2f
Arya-Shrivastava11 <aryashrivastava29@gmail.com>
2026-09-24 21:35:28 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,8 @@
+n=int(input("Enter no. strings and no's items in a list"))
+list1=[]
+for i in range(1,n+1):
+    a=input(f"Enter item{i}")
+    list1.append(a)
+for item in list1:
+   print(item,list1.index(item))
+    
