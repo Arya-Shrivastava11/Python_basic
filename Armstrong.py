bf79cfc8b0c986e21e636a54ed9a727602b2bcb1
Arya-Shrivastava11 <aryashrivastava29@gmail.com>
2026-09-26 17:08:57 +0530

    Add files via upload

---
+++ Armstrong.py
@@ -0,0 +1,16 @@
+input1=int(input("Enter a number : "))
+copy=input1
+str1=str.input1
+n=len(str1)
+rem=0
+sum1=0
+quotient
+while(copy!=0):
+    copy%10=rem
+    copy//3=qoutient
+    sum1=sum1+(rem)**(n)
+    qoutient=copy
+if sum1==input1:
+    print("No. is armstrong")
+else:
+    print("No. isn't armstrong")
